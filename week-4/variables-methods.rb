932e78f6c4c922a6e463795296f88f5ef8c31092
Bigless27 <lessonryan@gmail.com>
2015-09-09 22:34:50 -0400

    ref

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,4 +1,22 @@
+
+
+```ruby
 puts "What is your favoirite big number?"
 number=gets.chomp
 number2=number.to_i+1
 puts "You may like this bigger number more! " + number2.to_s
+```
+
+
+#Reflection:
+#-(variable name)=
+#-def(method name)(parameters)
+#end
+#-A local variable only contains variable and a method can contain anything from a variable to executable code. You can also pass multiple parameters through a method
+#-ruby (name of ruby file).rb. Make sure you are in the files directory
+#rspec (name of ruby file). Make sure you are in the files directory.
+#-Nothing was confusing about his material it was pretty straight forward.
+'-'https://github.com/Bigless27/Phase-0/blob/master/week-4/math/my_solution.rb
+'-'https://github.com/Bigless27/Phase-0/blob/master/week-4/address/my_solution.rb
+
+
