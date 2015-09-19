4a16eb15166ff681e37e3717fc19d06a84700735
Bigless27 <lessonryan@gmail.com>
2015-09-19 03:22:11 -0400

    m

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -70,8 +70,21 @@ end
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
   source.dup
-  source.delete_if {|x| x[1]==thing_to_delete}
+del=[]
+source.each do |item|
+    if item.is_a? Integer
+      del << item
+      next
+    elsif item.include?(thing_to_delete)
+      next
+    else
+      del << item
+    end
+  end
+  return source=del
 end
+
+
 # This method was extremely hard to figure out. The reason being is that most methods that check if an character is in an element only work for strings. As you can see in this array there are integers to. I could account for this in a non-destructive way but I can only think of one way to do it destructively.
 #For my method above I used the delete_if method and pointed each element itterated through it to a ceratin index. Since ever element that has an "a" in it has it in the same place this works.
 #This also accounts for the integers. Since the integers dont have a [1] index it returns nill. This can be compared in the delete_if code block.
@@ -91,19 +104,7 @@ end
 
 
 
-  # del=[]
-  # source.each do |item|
-#     if item.is_a? Integer
-#       del << item
-#       next
-#     elsif item.include?(thing_to_delete)
-#       next
-#     else
-#       del << item
-#     end
-#   end
-#   return source=del
-# end
+
 ############################################
 def my_hash_deletion_method!(source, thing_to_delete)
   source.dup
